5ddc22cfe3309ee1d5b541bb9654fc4893710dc1
agustindemiddi <agustindemiddi@gmail.com>
2023-11-23 08:44:03 -0300

    add localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import TaskList from './components/TaskList/TaskList';
 import TaskForm from './components/TaskForm/TaskForm';
 
-const DUMMY_LIST = [
-  { id: '1', name: 'a', isComplete: false },
-  { id: '2', name: 'b', isComplete: true },
-  { id: '3', name: 'c', isComplete: false },
-  { id: '4', name: 'd', isComplete: false },
-  { id: '5', name: 'e', isComplete: true },
-];
-
 const App = () => {
-  const [tasks, setTasks] = useState(DUMMY_LIST);
+  const storedTasks = localStorage.getItem('tasks');
+  const [tasks, setTasks] = useState(
+    storedTasks ? JSON.parse(storedTasks) : []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   const hanldeAddTask = (newTask) => {
     setTasks((prevTasks) => [newTask, ...prevTasks]);
